0f4c1bca8f3061b49f2c0be6e2e41e8f3bf74e65
Siyeop Yoon <45598938+siyeopyoon@users.noreply.github.com>
2025-04-22 10:31:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
 
     print (f"python app start at \n {time.gmtime()}")
 
-    # 외부 폴더 경로 설정
+    # set external foler
     external_folder_path = "/external_folder"
 
     print ('Connected "container folder" = "system Volume"')
 
-    # "output" 폴더 생성
+    # create the "output" folder
     output_folder_path = f"{external_folder_path}/Projects/hellocamca/output"
     os.makedirs(output_folder_path, exist_ok=True)
 
     print (f'Folder create for output at {output_folder_path}')
 
 
-    # "input" 폴더
+    # read from the foler "input" 
     input_folder_path = f"{external_folder_path}/Datasets/1_example"
     input_npy=np.load(f"{input_folder_path}/random.npy")
 
